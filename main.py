5c5c94049a860adea3e623be69866faae9cb5f6e
Niels <nielsfreelancer7750@gmail.com>
2024-02-21 11:06:29 +0100

    Fix even/uneven

--- main.py
+++ main.py
@@ -20,43 +20,49 @@ def grid_to_color(grid):
 alpha = 1.0
 beta = 0.4
 gamma = 0.001
-n = 51 # must be uneven
+n = 51  # must be uneven
 
-s = np.ones((n, n), dtype=float)*beta
+s = np.ones((n, n), dtype=float) * beta
 f = np.zeros((n, n), dtype=float)
-s[n // 2][n // 2] = 1
-'''
-s[rows//2][columns//2-1] = 0.7
-s[rows//2][columns//2+1] = 0.7
-s[rows//2-1][columns//2] = 0.7
-s[rows//2+1][columns//2] = 0.7
-s[rows//2+1][columns//2+1] = 0.7
-s[rows//2-1][columns//2+1] = 0.7
-'''
+mid = int((n - 1) / 2)
+s[mid][mid] = 1
 
 r = np.zeros((n, n), dtype=float)
 nr = np.zeros((n, n), dtype=float)
-av = np.zeros((n,n), dtype=float)
-for k in range(100):
+av = np.zeros((n, n), dtype=float)
+for k in range(125):
     r = r * 0
     nr = nr * 0
     av = av * 0
     for i in range(n):
         for j in range(n):
-            if s[i][j] >= 1 or s[i][(j - 1)%n] >= 1 or s[i][(j + 1)%n] >= 1 or s[(i - 1)%n][j] >= 1 or s[(i + 1)%n][j] >= 1 or s[(i + 1)%n][(j + 1)%n] >= 1 or s[(i - 1)%n][(j + 1)%n] >= 1:
+            if i%2==0 and s[i][j] >= 1 or s[i][(j - 1) % n] >= 1 or s[i][(j + 1) % n] >= 1 or s[(i - 1) % n][j] >= 1 or \
+                    s[(i + 1) % n][j] >= 1 or s[(i + 1) % n][(j - 1) % n] >= 1 or s[(i - 1) % n][(j - 1) % n] >= 1:
+                r[i][j] = s[i][j] + gamma
+            elif i%2 !=0 and s[i][j] >= 1 or s[i][(j - 1) % n] >= 1 or s[i][(j + 1) % n] >= 1 or s[(i - 1) % n][j] >= 1 or \
+                    s[(i + 1) % n][j] >= 1 or s[(i + 1) % n][(j + 1) % n] >= 1 or s[(i - 1) % n][(j + 1) % n] >= 1:
                 r[i][j] = s[i][j] + gamma
             else:
                 nr[i][j] = s[i][j]
 
     for i in range(n):
         for j in range(n):
-            av[i][j] = (nr[i][j]/2 +
-                        nr[i][(j - 1)%n]/12 +
-                        nr[i][(j + 1)%n]/12 +
-                        nr[(i - 1)%n][j]/12 +
-                        nr[(i + 1)%n][j]/12 +
-                        nr[(i + 1)%n][(j + 1)%n]/12 +
-                        nr[(i - 1)%n][(j + 1)%n]/12)
+            if i % 2 == 0:
+                av[i][j] = (nr[i][j] * (1 - alpha * 0.5) +
+                            nr[i][(j - 1) % n] * (alpha / 12) +
+                            nr[i][(j + 1) % n] * (alpha / 12) +
+                            nr[(i - 1) % n][j] * (alpha / 12) +
+                            nr[(i + 1) % n][j] * (alpha / 12) +
+                            nr[(i + 1) % n][(j - 1) % n] * (alpha / 12) +
+                            nr[(i - 1) % n][(j - 1) % n] * (alpha / 12))
+            else:
+                av[i][j] = (nr[i][j] * (1 - alpha * 0.5) +
+                            nr[i][(j - 1) % n] * (alpha / 12) +
+                            nr[i][(j + 1) % n] * (alpha / 12) +
+                            nr[(i - 1) % n][j] * (alpha / 12) +
+                            nr[(i + 1) % n][j] * (alpha / 12) +
+                            nr[(i + 1) % n][(j + 1) % n] * (alpha / 12) +
+                            nr[(i - 1) % n][(j + 1) % n] * (alpha / 12))
 
     s = r + av
     f = s >= 1
